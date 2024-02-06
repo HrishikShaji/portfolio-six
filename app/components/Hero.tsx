3dce5705563bd122eb9fb4ac77854a8fc11ce779
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-06 19:34:15 +0530

    added sampleCity

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,10 @@
+import { CityOne } from "./CityOne";
+import { SampleCity } from "./SampleCity";
+
 export const Hero = () => {
   return (
-    <div className="h-full relative w-[100vw] overflow-hidden flex justify-center items-center bg-orange-500 panel  "></div>
+    <div className="h-full relative w-[100vw] overflow-hidden flex justify-center items-center bg-orange-500 panel  ">
+      <SampleCity />
+    </div>
   );
 };
--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -11,62 +11,62 @@ import { Contact } from "./Contact";
 import { Sun } from "./Sun";
 
 export const Wrapper = () => {
-	const containerRef = useRef<HTMLDivElement>(null);
-	const targetRef = useRef<HTMLDivElement>(null);
-	const sunRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+  const sunRef = useRef<HTMLDivElement>(null);
 
-	useGSAP(() => {
-		if (containerRef.current && targetRef.current && sunRef.current) {
-			const amountToScroll = targetRef.current.offsetWidth - window.innerWidth;
+  useGSAP(() => {
+    if (containerRef.current && targetRef.current && sunRef.current) {
+      const amountToScroll = targetRef.current.offsetWidth - window.innerWidth;
 
-			gsap.to(targetRef.current, {
-				x: -amountToScroll,
-				duration: 2,
-				scrollTrigger: {
-					trigger: containerRef.current,
-					pin: containerRef.current,
-					start: "left top",
-					end: "+=" + amountToScroll,
-					scrub: 1,
-				},
-			});
+      gsap.to(targetRef.current, {
+        x: -amountToScroll,
+        duration: 2,
+        scrollTrigger: {
+          trigger: containerRef.current,
+          pin: containerRef.current,
+          start: "left top",
+          end: "+=" + amountToScroll,
+          scrub: 1,
+        },
+      });
 
-			const tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: sunRef.current,
-					start: "left top",
-					end: "+=" + amountToScroll,
-					scrub: true,
-					markers: true,
-				},
-			});
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sunRef.current,
+          start: "left top",
+          end: "+=" + amountToScroll,
+          scrub: 1,
+          markers: true,
+        },
+      });
 
-			tl.to(sunRef.current, {
-				motionPath: {
-					path: [
-						{ x: window.innerWidth / 2, y: -100 },
-						{ x: window.innerWidth, y: 0 },
-					],
-				},
-			});
-		}
-	}, {});
-	return (
-		<div
-			ref={containerRef}
-			className="flex relative flex-nowrap h-[100vh] w-full"
-		>
-			<div
-				ref={sunRef}
-				className="w-40 fixed h-40 -translate-x-[50%] rounded-full bg-black top-40 left-0 z-20"
-			></div>
-			<div className="flex gap-0 m-0" ref={targetRef}>
-				<Hero />
-				<About />
-				<Skills />
-				<Projects />
-				<Contact />
-			</div>
-		</div>
-	);
+      tl.to(sunRef.current, {
+        motionPath: {
+          path: [
+            { x: window.innerWidth / 2, y: -100 },
+            { x: window.innerWidth, y: 0 },
+          ],
+        },
+      });
+    }
+  }, {});
+  return (
+    <div
+      ref={containerRef}
+      className="flex relative flex-nowrap h-[100vh] w-full"
+    >
+      <div
+        ref={sunRef}
+        className="w-40 fixed h-40 -translate-x-[50%] rounded-full bg-black top-40 left-0 z-20"
+      ></div>
+      <div className="flex gap-0 m-0" ref={targetRef}>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    </div>
+  );
 };
