de48471319f325af71f63e449f01d25556586c47
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-07 23:14:46 +0530

    added ref for the tween

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,15 +1,23 @@
+import { RefObject, useEffect } from "react";
 import { data } from "../lib/data";
 
-export const About = () => {
-	return (
-		<div className="h-full w-[100vw]  relative p-10 bg-orange-500 panel flex flex-col gap-10">
-			<h1 className="text-9xl">ABOUT ME</h1>
-			<p className="text-2xl">{data.about.description}</p>
-			<div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[100px] mix-blend-difference"></div>
-			<div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[300px] mix-blend-difference"></div>
+interface AboutProps {
+  animation: any;
+}
 
-			<div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[500px] mix-blend-difference"></div>
-			<div className="absolute w-full h-full z-20 flex bottom-0 left-0"></div>
-		</div>
-	);
+export const About: React.FC<AboutProps> = ({ animation }) => {
+  useEffect(() => {
+    console.log(animation);
+  }, [animation]);
+  return (
+    <div className="h-full w-[100vw]  relative p-10 bg-orange-500 panel flex flex-col gap-10">
+      <h1 className="text-9xl">ABOUT ME</h1>
+      <p className="text-2xl">{data.about.description}</p>
+      <div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[100px] mix-blend-difference"></div>
+      <div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[300px] mix-blend-difference"></div>
+
+      <div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[500px] mix-blend-difference"></div>
+      <div className="absolute w-full h-full z-20 flex bottom-0 left-0"></div>
+    </div>
+  );
 };
--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -12,142 +12,152 @@ import SampleCity from "./SampleCity";
 import { animate } from "framer-motion/dom";
 
 export const Wrapper = () => {
-	const containerRef = useRef<HTMLDivElement>(null);
-	const targetRef = useRef<HTMLDivElement>(null);
-	const sunRef = useRef<HTMLDivElement>(null);
-	const oneRef = useRef<SVGSVGElement>(null);
-	const twoRef = useRef<SVGSVGElement>(null);
-	const threeRef = useRef<SVGSVGElement>(null);
-	const fourRef = useRef<SVGSVGElement>(null);
-	const fiveRef = useRef<SVGSVGElement>(null);
-	const sixRef = useRef<SVGSVGElement>(null);
-	const sevenRef = useRef<SVGSVGElement>(null);
-	const nineRef = useRef<SVGSVGElement>(null);
-	const eightRef = useRef<SVGSVGElement>(null);
-	const tenRef = useRef<SVGSVGElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+  const sunRef = useRef<HTMLDivElement>(null);
+  const oneRef = useRef<SVGSVGElement>(null);
+  const twoRef = useRef<SVGSVGElement>(null);
+  const threeRef = useRef<SVGSVGElement>(null);
+  const fourRef = useRef<SVGSVGElement>(null);
+  const fiveRef = useRef<SVGSVGElement>(null);
+  const sixRef = useRef<SVGSVGElement>(null);
+  const sevenRef = useRef<SVGSVGElement>(null);
+  const nineRef = useRef<SVGSVGElement>(null);
+  const eightRef = useRef<SVGSVGElement>(null);
+  const tenRef = useRef<SVGSVGElement>(null);
 
-	const cityContainer = useRef<HTMLDivElement>(null);
-	const cityRefs = useRef<HTMLDivElement[]>([]);
-	cityRefs.current = [];
+  const cityContainer = useRef<HTMLDivElement>(null);
+  const cityRefs = useRef<HTMLDivElement[]>([]);
+  cityRefs.current = [];
 
-	useGSAP(
-		() => {
-			if (containerRef.current && targetRef.current && sunRef.current) {
-				const amountToScroll =
-					targetRef.current.offsetWidth - window.innerWidth;
+  const scrollTween = useRef({});
 
-				const animation = gsap.to([targetRef.current, cityContainer.current], {
-					x: -amountToScroll,
-					duration: 2,
-					ease: "none",
-					scrollTrigger: {
-						trigger: containerRef.current,
-						pin: containerRef.current,
-						start: "left top",
-						end: "+=" + amountToScroll,
-						scrub: true,
-					},
-				});
+  useGSAP(
+    () => {
+      if (
+        containerRef.current &&
+        scrollTween.current &&
+        targetRef.current &&
+        sunRef.current
+      ) {
+        const amountToScroll =
+          targetRef.current.offsetWidth - window.innerWidth;
 
-				const tl = gsap.timeline({
-					scrollTrigger: {
-						trigger: sunRef.current,
-						start: "left top",
-						end: "+=" + amountToScroll,
-						scrub: true,
-					},
-				});
+        scrollTween.current = gsap.to(
+          [targetRef.current, cityContainer.current],
+          {
+            x: -amountToScroll,
+            duration: 2,
+            ease: "none",
+            scrollTrigger: {
+              trigger: containerRef.current,
+              pin: containerRef.current,
+              start: "left top",
+              end: "+=" + amountToScroll,
+              scrub: true,
+            },
+          },
+        );
 
-				tl.to(sunRef.current, {
-					motionPath: {
-						path: [
-							{ x: window.innerWidth / 2, y: -150 },
-							{ x: window.innerWidth, y: 0 },
-						],
-					},
-				});
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: sunRef.current,
+            start: "left top",
+            end: "+=" + amountToScroll,
+            scrub: true,
+          },
+        });
 
-				if (cityContainer.current) {
-					const cityAnimation = (ref: SVGSVGElement) => {
-						const paths = Array.from(ref.getElementsByClassName("city-one"));
+        tl.to(sunRef.current, {
+          motionPath: {
+            path: [
+              { x: window.innerWidth / 2, y: -150 },
+              { x: window.innerWidth, y: 0 },
+            ],
+          },
+        });
 
-						paths.forEach((path) => {
-							gsap.to(path, {
-								fill: "blue",
-								scrollTrigger: {
-									containerAnimation: animation,
-									trigger: ref,
-									start: "left 80%",
-									end: "left 25%",
-									scrub: true,
-									markers: true,
-								},
-							});
-						});
-					};
+        if (cityContainer.current) {
+          const cityAnimation = (ref: SVGSVGElement) => {
+            const paths = Array.from(ref.getElementsByClassName("city-one"));
 
-					if (
-						oneRef.current &&
-						twoRef.current &&
-						threeRef.current &&
-						fourRef.current &&
-						fiveRef.current &&
-						sixRef.current &&
-						sevenRef.current &&
-						eightRef.current &&
-						nineRef.current &&
-						tenRef.current
-					) {
-						cityAnimation(oneRef.current);
-						cityAnimation(twoRef.current);
-						cityAnimation(threeRef.current);
-						cityAnimation(fourRef.current);
-						cityAnimation(fiveRef.current);
-						cityAnimation(sixRef.current);
-						cityAnimation(sevenRef.current);
-						cityAnimation(eightRef.current);
-						cityAnimation(nineRef.current);
-						cityAnimation(tenRef.current);
-					}
-				}
-			}
-		},
-		{ scope: containerRef },
-	);
+            paths.forEach((path) => {
+              gsap.to(path, {
+                fill: "blue",
+                scrollTrigger: {
+                  containerAnimation: scrollTween.current,
+                  trigger: ref,
+                  start: "left 80%",
+                  end: "left 25%",
+                  scrub: true,
+                  markers: true,
+                },
+              });
+            });
+          };
 
-	return (
-		<div
-			ref={containerRef}
-			className="flex relative
+          if (
+            oneRef.current &&
+            twoRef.current &&
+            threeRef.current &&
+            fourRef.current &&
+            fiveRef.current &&
+            sixRef.current &&
+            sevenRef.current &&
+            eightRef.current &&
+            nineRef.current &&
+            tenRef.current
+          ) {
+            cityAnimation(oneRef.current);
+            cityAnimation(twoRef.current);
+            cityAnimation(threeRef.current);
+            cityAnimation(fourRef.current);
+            cityAnimation(fiveRef.current);
+            cityAnimation(sixRef.current);
+            cityAnimation(sevenRef.current);
+            cityAnimation(eightRef.current);
+            cityAnimation(nineRef.current);
+            cityAnimation(tenRef.current);
+          }
+        }
+      }
+    },
+    { scope: containerRef, dependencies: [scrollTween.current] },
+  );
+
+  return (
+    <div
+      ref={containerRef}
+      className="flex relative
 			flex-nowrap h-[100vh] w-full"
-		>
-			<div
-				ref={sunRef}
-				className="w-20 fixed h-20 -translate-x-[50%] rounded-full bg-orange-500 top-40 left-0 mix-blend-difference z-20"
-			></div>
+    >
+      <div
+        ref={sunRef}
+        className="w-20 fixed h-20 -translate-x-[50%] rounded-full bg-orange-500 top-40 left-0 mix-blend-difference z-20"
+      ></div>
 
-			<div
-				ref={cityContainer}
-				className="absolute flex z-20 top-40 left-0 w-[500vw] h-[600px]"
-			>
-				<SampleCity ref={oneRef} />
-				<SampleCity ref={twoRef} />
-				<SampleCity ref={threeRef} />
-				<SampleCity ref={fourRef} />
-				<SampleCity ref={fiveRef} />
-				<SampleCity ref={sixRef} />
-				<SampleCity ref={sevenRef} />
-				<SampleCity ref={eightRef} />
-				<SampleCity ref={nineRef} />
-				<SampleCity ref={tenRef} />
-			</div>
-			<div className="flex relative gap-0 m-0 " ref={targetRef}>
-				<Hero />
-				<About />
-				<Skills />
-				<Projects />
-				<Contact />
-			</div>
-		</div>
-	);
+      <div
+        ref={cityContainer}
+        className="absolute flex z-20 top-40 left-0 w-[500vw] h-[600px]"
+      >
+        <SampleCity ref={oneRef} />
+        <SampleCity ref={twoRef} />
+        <SampleCity ref={threeRef} />
+        <SampleCity ref={fourRef} />
+        <SampleCity ref={fiveRef} />
+        <SampleCity ref={sixRef} />
+        <SampleCity ref={sevenRef} />
+        <SampleCity ref={eightRef} />
+        <SampleCity ref={nineRef} />
+        <SampleCity ref={tenRef} />
+      </div>
+      <div className="flex relative gap-0 m-0 " ref={targetRef}>
+        <Hero />
+        <About animation={scrollTween.current} />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    </div>
+  );
 };
