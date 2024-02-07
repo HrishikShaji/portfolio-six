39eaf85aeb784a8965f43a66f0d111fb7a3ff015
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-07 20:36:15 +0530

    container animation bug fixed

--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -2,22 +2,14 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import {
-	LegacyRef,
-	ReactNode,
-	RefObject,
-	useEffect,
-	useRef,
-	useState,
-} from "react";
+import { useRef } from "react";
 import { Hero } from "./Hero";
 import { About } from "./About";
 import { Skills } from "./Skills";
 import { Projects } from "./Projects";
 import { Contact } from "./Contact";
-import { Sun } from "./Sun";
-import { userAgentFromString } from "next/server";
 import SampleCity from "./SampleCity";
+import { animate } from "framer-motion/dom";
 
 export const Wrapper = () => {
 	const containerRef = useRef<HTMLDivElement>(null);
@@ -34,21 +26,20 @@ export const Wrapper = () => {
 	const eightRef = useRef<SVGSVGElement>(null);
 	const tenRef = useRef<SVGSVGElement>(null);
 
-	const heroRef = useRef<HTMLDivElement>(null);
+	const cityContainer = useRef<HTMLDivElement>(null);
 	const cityRefs = useRef<HTMLDivElement[]>([]);
 	cityRefs.current = [];
 
 	useGSAP(
 		() => {
-			const pathOne = document.getElementById("path-one");
 			if (containerRef.current && targetRef.current && sunRef.current) {
-				console.log(cityRefs.current);
 				const amountToScroll =
 					targetRef.current.offsetWidth - window.innerWidth;
 
-				const animation = gsap.to(targetRef.current, {
+				const animation = gsap.to([targetRef.current, cityContainer.current], {
 					x: -amountToScroll,
 					duration: 2,
+					ease: "none",
 					scrollTrigger: {
 						trigger: containerRef.current,
 						pin: containerRef.current,
@@ -75,48 +66,49 @@ export const Wrapper = () => {
 						],
 					},
 				});
-				const cities = document.getElementsByClassName("sample-city");
 
-				const cityAnimation = (ref: SVGSVGElement) => {
-					const paths = Array.from(ref.getElementsByClassName("city-one"));
+				if (cityContainer.current) {
+					const cityAnimation = (ref: SVGSVGElement) => {
+						const paths = Array.from(ref.getElementsByClassName("city-one"));
 
-					paths.forEach((path) => {
-						gsap.to(path, {
-							fill: "blue",
-							scrollTrigger: {
-								containerAnimation: animation,
-								trigger: ref,
-								start: "left 50%",
-								end: "left 30%",
-								scrub: true,
-								markers: true,
-							},
+						paths.forEach((path) => {
+							gsap.to(path, {
+								fill: "blue",
+								scrollTrigger: {
+									containerAnimation: animation,
+									trigger: ref,
+									start: "left 80%",
+									end: "left 25%",
+									scrub: true,
+									markers: true,
+								},
+							});
 						});
-					});
-				};
+					};
 
-				if (
-					oneRef.current &&
-					twoRef.current &&
-					threeRef.current &&
-					fourRef.current &&
-					fiveRef.current &&
-					sixRef.current &&
-					sevenRef.current &&
-					eightRef.current &&
-					nineRef.current &&
-					tenRef.current
-				) {
-					cityAnimation(oneRef.current);
-					cityAnimation(twoRef.current);
-					cityAnimation(threeRef.current);
-					cityAnimation(fourRef.current);
-					cityAnimation(fiveRef.current);
-					cityAnimation(sixRef.current);
-					cityAnimation(sevenRef.current);
-					cityAnimation(eightRef.current);
-					cityAnimation(nineRef.current);
-					cityAnimation(tenRef.current);
+					if (
+						oneRef.current &&
+						twoRef.current &&
+						threeRef.current &&
+						fourRef.current &&
+						fiveRef.current &&
+						sixRef.current &&
+						sevenRef.current &&
+						eightRef.current &&
+						nineRef.current &&
+						tenRef.current
+					) {
+						cityAnimation(oneRef.current);
+						cityAnimation(twoRef.current);
+						cityAnimation(threeRef.current);
+						cityAnimation(fourRef.current);
+						cityAnimation(fiveRef.current);
+						cityAnimation(sixRef.current);
+						cityAnimation(sevenRef.current);
+						cityAnimation(eightRef.current);
+						cityAnimation(nineRef.current);
+						cityAnimation(tenRef.current);
+					}
 				}
 			}
 		},
@@ -134,19 +126,22 @@ export const Wrapper = () => {
 				className="w-20 fixed h-20 -translate-x-[50%] rounded-full bg-orange-500 top-40 left-0 mix-blend-difference z-20"
 			></div>
 
+			<div
+				ref={cityContainer}
+				className="absolute flex z-20 top-40 left-0 w-[500vw] h-[600px]"
+			>
+				<SampleCity ref={oneRef} />
+				<SampleCity ref={twoRef} />
+				<SampleCity ref={threeRef} />
+				<SampleCity ref={fourRef} />
+				<SampleCity ref={fiveRef} />
+				<SampleCity ref={sixRef} />
+				<SampleCity ref={sevenRef} />
+				<SampleCity ref={eightRef} />
+				<SampleCity ref={nineRef} />
+				<SampleCity ref={tenRef} />
+			</div>
 			<div className="flex relative gap-0 m-0 " ref={targetRef}>
-				<div className="absolute flex z-20 top-40 left-0 w-[500vw] h-[600px]">
-					<SampleCity ref={oneRef} />
-					<SampleCity ref={twoRef} />
-					<SampleCity ref={threeRef} />
-					<SampleCity ref={fourRef} />
-					<SampleCity ref={fiveRef} />
-					<SampleCity ref={sixRef} />
-					<SampleCity ref={sevenRef} />
-					<SampleCity ref={eightRef} />
-					<SampleCity ref={nineRef} />
-					<SampleCity ref={tenRef} />
-				</div>
 				<Hero />
 				<About />
 				<Skills />
