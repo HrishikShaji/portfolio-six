e4f9083a7d016cd8f300baf2af317674390dbeab
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-08 17:33:11 +0530

    sample city added

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -4,79 +4,64 @@ import { data } from "../lib/data";
 import SampleCity from "./SampleCity";
 import gsap from "gsap";
 import { ScrollContext } from "./ScrollContext";
+import { SampleCityTwo } from "./SampleCityTwo";
 
 export const About = () => {
-  const refOne = useRef<SVGSVGElement>(null);
-  const refTwo = useRef<SVGSVGElement>(null);
-  const mainRef = useRef<HTMLDivElement>(null);
+	const refOne = useRef<SVGSVGElement>(null);
+	const refTwo = useRef<SVGSVGElement>(null);
+	const mainRef = useRef<HTMLDivElement>(null);
 
-  const refs = useContext(ScrollContext);
+	const refs = useContext(ScrollContext);
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      if (
-        refOne.current &&
-        refTwo.current &&
-        mainRef.current &&
-        refs &&
-        refs.scrollTween.current
-      ) {
-        const { scrollTween } = refs;
-        const paths = Array.from(
-          refOne.current.getElementsByClassName("city-one"),
-        );
-        const pathsTwo = Array.from(
-          refTwo.current.getElementsByClassName("city-one"),
-        );
-        paths.forEach((path) => {
-          gsap.to(path, {
-            ease: "none",
-            fill: "blue",
-            scrollTrigger: {
-              containerAnimation: scrollTween.current,
-              trigger: refOne.current,
-              start: "left 90%",
-              end: "left 40%",
-              markers: true,
-              scrub: true,
-            },
-          });
-        });
-        pathsTwo.forEach((path) => {
-          gsap.to(path, {
-            ease: "none",
-            fill: "blue",
-            scrollTrigger: {
-              containerAnimation: scrollTween.current,
-              trigger: refTwo.current,
-              start: "left 90%",
-              end: "left 40%",
-              markers: true,
-              scrub: true,
-            },
-          });
-        });
-      }
-    }, mainRef);
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			if (
+				refOne.current &&
+				refTwo.current &&
+				mainRef.current &&
+				refs &&
+				refs.scrollTween.current
+			) {
+				const { scrollTween } = refs;
+				const paths = Array.from(
+					refOne.current.getElementsByClassName("city-one"),
+				);
+				const pathsTwo = Array.from(
+					refTwo.current.getElementsByClassName("city-one"),
+				);
+				gsap.to(".city-one", {
+					stagger: 2,
+					ease: "none",
+					fill: "blue",
+					scrollTrigger: {
+						containerAnimation: scrollTween.current,
+						trigger: refOne.current,
+						start: "left 50%",
+						end: "left 20%",
+						markers: true,
+						scrub: true,
+					},
+				});
+			}
+		}, mainRef);
 
-    return () => ctx.kill();
-  }, [refs]);
+		return () => ctx.kill();
+	}, [refs]);
 
-  return (
-    <div
-      ref={mainRef}
-      className="h-full w-[100vw]  relative p-10 bg-orange-500 panel flex flex-col gap-10"
-    >
-      <h1 className="text-9xl">ABOUT ME</h1>
-      <p className="text-2xl">{data.about.description}</p>
-      <div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[100px] mix-blend-difference"></div>
-      <div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[300px] mix-blend-difference"></div>
+	return (
+		<div
+			ref={mainRef}
+			className="h-full w-[100vw]  relative p-10 bg-orange-500 panel flex flex-col gap-10"
+		>
+			<h1 className="text-9xl">ABOUT ME</h1>
+			<p className="text-2xl">{data.about.description}</p>
+			<div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[100px] mix-blend-difference"></div>
+			<div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[300px] mix-blend-difference"></div>
 
-      <div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[500px] mix-blend-difference"></div>
-      <div className="absolute w-full h-full z-20 flex bottom-0 left-0">
-        <SampleCity ref={refOne} />
-        <SampleCity ref={refTwo} />
-      </div>
-    </div>
-  );
+			<div className="h-[140vh] absolute w-[120px] bg-orange-500 rotate-[30deg] -top-40 right-[500px] mix-blend-difference"></div>
+			<div className="absolute w-[100vw] h-[50%] z-20 flex bottom-0 left-0">
+				<SampleCityTwo />
+			</div>
+		</div>
+	);
 };
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,14 +1,68 @@
+import { useContext, useEffect, useRef } from "react";
 import { data } from "../lib/data";
+import { ScrollContext } from "./ScrollContext";
+import SampleCity from "./SampleCity";
+import gsap from "gsap";
 
 export const Skills = () => {
-	return (
-		<div className="h-full w-[100vw] bg-orange-500 flex flex-col gap-10 panel p-10">
-			<h1 className="text-9xl">MY SKILLS</h1>
-			<div className="w-full grid grid-cols-4 gap-4">
-				{data.skills.data.map((item, i) => (
-					<div key={i}><h1 className="text-6xl">{item}</h1></div>
-				))}
-			</div>
-		</div>
-	);
+  const refOne = useRef<SVGSVGElement>(null);
+  const refTwo = useRef<SVGSVGElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const refs = useContext(ScrollContext);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      if (
+        refOne.current &&
+        refTwo.current &&
+        mainRef.current &&
+        refs &&
+        refs.scrollTween.current
+      ) {
+        const { scrollTween } = refs;
+        const paths = Array.from(
+          refOne.current.getElementsByClassName("city-one"),
+        );
+        const pathsTwo = Array.from(
+          refTwo.current.getElementsByClassName("city-one"),
+        );
+        gsap.to(".city-one", {
+          stagger: 2,
+          ease: "none",
+          fill: "blue",
+          scrollTrigger: {
+            containerAnimation: scrollTween.current,
+            trigger: refOne.current,
+            start: "left 50%",
+            end: "left 20%",
+            markers: true,
+            scrub: true,
+          },
+        });
+      }
+    }, mainRef);
+
+    return () => ctx.kill();
+  }, [refs]);
+
+  return (
+    <div
+      ref={mainRef}
+      className=" relative h-full w-[100vw] bg-orange-500 flex flex-col gap-10 panel p-10"
+    >
+      <h1 className="text-9xl">MY SKILLS</h1>
+      <div className="w-full grid grid-cols-4 gap-4">
+        {data.skills.data.map((item, i) => (
+          <div key={i}>
+            <h1 className="text-6xl">{item}</h1>
+          </div>
+        ))}
+      </div>
+      <div className="absolute w-full h-full z-20 flex bottom-0 left-0">
+        <SampleCity ref={refOne} />
+        <SampleCity ref={refTwo} />
+      </div>
+    </div>
+  );
 };
